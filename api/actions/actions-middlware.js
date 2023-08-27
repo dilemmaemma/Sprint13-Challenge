dfea80aeac45b302771d4d0642d60ef023a6e3e3
delimmaemma <calvinhtml@gmail.com>
2023-08-26 20:41:52 -0500

    Still having issues

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -25,42 +25,40 @@ async function validateActionId (req, res, next) {
 }
 
 function validateAction(req, res, next) {
-    Action.get()
-        .then(id => {
-            const { project_id, description, notes, completed } = req.body
-            if(!id.includes(project_id)) {
+    const { project_id, description, notes, completed } = req.body;
+
+    Action.get(req.params.id)
+        .then(actions => {
+            const existingProjectIds = actions.map(action => action.project_id);
+            console.log(existingProjectIds)
+
+            if (!existingProjectIds.includes(project_id)) {
                 res.status(400).json({
                     message: 'project id must correspond with existing project'
-                })
+                });
+            } else if (!project_id || !project_id.trim() || !description || !notes) {
+                res.status(400).json({
+                    message: 'missing required fields'
+                });
+            } else if (description.length > 128) {
+                res.status(400).json({
+                    message: 'description must be 128 characters or less'
+                });
+            } else if (completed !== true && completed !== false) {
+                res.status(400).json({
+                    message: 'Missing completed property'
+                });
             } else {
-                if (!project_id || !project_id.trim() || !description || !notes) {
-                    res.status(400).json({
-                        message: 'missing required fields'
-                    })
-                } else if (description.length > 128) {
-                    res.status(400).json({
-                        message: 'description must be 128 characters or less'
-                    })
-                } else if (completed !== true && completed !== false) {
-                    res.status(400).json({
-                        message: 'Missing completed property'
-                    })
-                } else {
-                    req.project_id = project_id.trim()
-                    req.description = description
-                    req.notes = notes
-                    req.completed = completed
-                    next()
-                }
+                req.project_id = project_id.trim();
+                req.description = description;
+                req.notes = notes;
+                req.completed = completed;
+                next();
             }
         })
-        .catch (err => {
-            res.status(400).json({
-                message: err.message,
-                stack: err.stack
-            })
-        })
+        .catch(err => next(err));
 }
+
 
 module.exports = {
     logger,
